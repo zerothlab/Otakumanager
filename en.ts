4a5ff24c6576dcf2f7b94ba59c1e112863d51105
Bauce K <zerothlab@users.noreply.github.com>
2018-10-13 18:18:54 +0800

    ...Nothing

---
+++ en.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="27"/>
+        <source>Hello Otakumanager!
+--Bauce K</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="41"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="56"/>
+        <source>Hello Otakumanager</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="75"/>
+        <source>K</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="80"/>
+        <source>A AVG Manager</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ zh_CN.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="27"/>
+        <source>Hello Otakumanager!
+--Bauce K</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="41"/>
+        <source>Close</source>
+        <translation>关闭</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="56"/>
+        <source>Hello Otakumanager</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="75"/>
+        <source>K</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="80"/>
+        <source>A AVG Manager</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
